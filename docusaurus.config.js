acea932f399e0304b123aaa74f6c46113ce443b6
Kunal Walavalkar <kunalw2002@gmail.com>
2024-02-20 15:58:32 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Kunal Walavalkar',
+  title: 'Blog',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/play.png',
 
