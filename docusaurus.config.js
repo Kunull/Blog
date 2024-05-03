33ffd83e437d994d12bff5ed7a4223985509ac2a
Kunal W <kunalw2002@gmail.com>
2024-05-03 13:25:02 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Blog | null',
+        title: 'Blog | Kunull',
         logo: {
           alt: 'My Site Logo',
           src: 'img/null_medium.png',
