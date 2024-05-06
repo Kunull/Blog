1d26b3cfd5761df2814c774584bd9085685b375a
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-06 20:06:42 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
       //   copyright: `Copyright © ${new Date().getFullYear()} Kunal Walavalkar.`,
       // },
       prism: {
-        theme: prismThemes.okaidia,
+        theme: prismThemes.gruvboxMaterialDark,
       },
 
       
