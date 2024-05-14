5f6dcc10f57a60fbb87ab8250d9fa65aeb44aed4
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-14 08:47:44 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/null_medium.png',
 
   // Set the production url of your site here
-  url: 'https://writeups-kunull.vercel.app',
+  url: 'https://blog-kunull.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
